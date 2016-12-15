b50c7f66f2a67c04c5ed29a66f157de202f1afbf
Chris Hayen <chris.hayen@performancehorizon.com>
2016-12-14 21:22:45 -0500

    Parsing image and doing a shitty store by reds

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/jpeg"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("go")
+
+	file, _ := os.Open("test.jpg")
+	image, err := jpeg.Decode(file)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sortByReds(image)
+}
+
+func sortByReds(image image.Image) {
+	size := image.Bounds().Size()
+	xRange := make([]int, size.X)
+	yRange := make([]int, size.Y)
+
+	pixelIndex := make(map[uint32]color.Color)
+
+	count := 0
+
+	for xIndex := range xRange {
+		for yIndex := range yRange {
+			x := xIndex + 1
+			y := yIndex + 1
+
+			color := image.At(x, y)
+			r, _, _, _ := color.RGBA()
+
+			pixelIndex[r] = color
+			count++
+		}
+	}
+
+	fmt.Println(len(pixelIndex), count)
+
+}
+
+func write() {
 }
